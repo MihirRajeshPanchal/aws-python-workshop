abfda66a6151c8143585895177c6a3a9db4ba337
Ved <vedbulsara7@gmail.com>
2024-06-11 00:38:39 +0530

    update home-page

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -5,8 +5,9 @@ import Mihir from "../assets/mihir.jpeg";
 const HomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold mb-8">AWS Python Mumpy Workshop</h1>
-
+      <h1 className="text-3xl font-bold mb-8"> <u>Leveraging Boto3</u>: Pythonic access to S3 and SNS on AWS</h1>
+      <br />
+                      
       <div className="flex flex-wrap justify-center gap-8">
         <div className="max-w-sm rounded-lg overflow-hidden shadow-md transform -rotate-3 hover:scale-105 transition-transform duration-300 ease-in-out">
           <img
@@ -27,16 +28,14 @@ const HomePage = () => {
         <div className="my-8"></div>
 
         <p className="mt-8 text-center text-gray-700 max-w-md overflow-hidden word-wrap-break-word">
-          Description of the event goes here. Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua.
+         <b>In this session, we will be exploring the Boto3 library in Python to interact with AWS services like S3 and SNS.</b> 
           <div className="flex justify-center mt-8">
-            <a href="http://">
+            <a href="https://bit.ly/mumpy-github" target="_blank">
               <button className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 hover:bg-gradient-to-r hover:from-purple-500 hover:via-pink-600 hover:to-red-600 text-white font-bold py-2 px-6 rounded-full shadow-lg mr-4">
                 GitHub
               </button>
             </a>
-            <a href="http://">
+            <a href="https://www.canva.com/design/DAGHrIrtLQs/iJTDUe83mudcY4o4gj_OZg/edit?utm_content=DAGHrIrtLQs&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target="_blank">
               <button className="bg-gradient-to-r from-green-400 to-blue-500 hover:bg-gradient-to-r hover:from-green-500 hover:to-blue-600 text-white font-bold py-2 px-6 rounded-full shadow-lg">
                 Presentation Link
               </button>
